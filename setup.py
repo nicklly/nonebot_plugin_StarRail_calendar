270fde132172442e39121b5ef8f14bdfbd025a8f
nicklly <1134741727@qq.com>
2023-05-01 22:04:25 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="nonebot_plugin_genshin_calendar",
-    version="0.0.3",
-    keywords=["pip", "nonebot_plugin_genshin_calendar"],
-    description="查看原神活动日历",
-    long_description="查看原神活动日历",
+    name="nonebot_plugin_star_rail_calendar",
+    version="0.0.1",
+    keywords=["pip", "nonebot_plugin_star_rail_calendar"],
+    description="查看《崩坏：星穹铁道》官方活动",
+    long_description="查看《崩坏：星穹铁道》官方活动",
     license="GPL Licence",
-    url="https://github.com/nicklly/nonebot_plugin_genshin_calendar",
+    url="https://github.com/nicklly/nonebot_plugin_star_rail_calendar",
     author="TonyKun",
     author_email="1134741727@qq.com",
     packages=find_packages(),
     include_package_data=True,
     platforms="any",
     install_requires=[
-        "Pillow >= 6.2.1",
         "nonebot2 >= 2.0.0b1",
         "nonebot-adapter-onebot >= 2.0.0b1",
     ],
