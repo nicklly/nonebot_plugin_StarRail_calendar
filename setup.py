b8d8874ae813f12f2b6ada3b26376a08fef8163f
nicklly <1134741727@qq.com>
2023-05-02 09:43:53 +0800

    更新部分依赖

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nonebot_plugin_star_rail_calendar",
-    version="0.0.1",
+    version="1.0.1",
     keywords=["pip", "nonebot_plugin_star_rail_calendar"],
     description="查看《崩坏：星穹铁道》官方活动",
     long_description="查看《崩坏：星穹铁道》官方活动",
@@ -13,11 +13,15 @@ setup(
     url="https://github.com/nicklly/nonebot_plugin_star_rail_calendar",
     author="TonyKun",
     author_email="1134741727@qq.com",
-    packages=find_packages(include=["template","template.*"]),
+    packages=find_packages(include=["template", "template.*"]),
     include_package_data=True,
     platforms="any",
     install_requires=[
         "nonebot2 >= 2.0.0b1",
         "nonebot-adapter-onebot >= 2.0.0b1",
+        "playwright == 1.32.1",
+        "httpx == 0.23.3",
+        "apscheduler == 3.10.1",
+        'jinja2 == 3.1.2'
     ],
 )
