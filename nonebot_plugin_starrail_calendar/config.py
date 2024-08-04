04608a36c936cd3845b85842c73f28cfa512d4b2
鱼酱 <94740235+kawaiior@users.noreply.github.com>
2024-08-04 17:36:49 +0800

    Update config.py
    Pydantic 版本 2.x 对模型字段声明的处理方式发生了变化。Pydantic 现在要求模型类中的所有属性都需要进行类型注解。

--- nonebot_plugin_starrail_calendar/config.py
+++ nonebot_plugin_starrail_calendar/config.py
@@ -4,9 +4,9 @@ from nonebot import get_driver
 
 class PluginConfig(BaseModel):
     # 页面宽度
-    width = 600
+    width: int = 600
     # 页面高度
-    height = 10
+    height: int = 10
     # 浏览器内核
     browser_type = 'firefox'
 
